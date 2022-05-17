18ef49a11809450ba68d23223c4c1ff705897e18
gabrielpaval <dan.paval@student.usv.ro>
2022-05-17 14:25:41 +0300

    Tema acasa laborator 2-creare clase necesare modelarii entitatilor

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -6,7 +6,32 @@ namespace Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instantierea unui obiect de tip Carte utilizand constructorul fara parametrii
+
+            var ob1 = new Carte();
+            string s1 = ob1.Info();
+            Console.WriteLine(s1);
+
+            //Instantierea unui obiect de tip Carte utilizand constructorul cu parametrii
+
+            Carte ob2 = new Carte("Mandrie si Prejudecata", "Jane Austin", "Corint", 2014, 3);
+            string s2 = ob2.Info();
+            Console.WriteLine(s2);
+
+            //Instantierea unui obiect de tip Persoana utilizand constructorul fara parametrii
+
+            var ob3 = new Persoana();
+            string s3 = ob3.InfoPersoana();
+            Console.WriteLine(s3);
+
+            //Instantierea unui obiect de tip Carte utilizand constructorul cu parametrii
+
+            Persoana ob4 = new Persoana("Paval", "Gabriel", 22, 0, "0746987884", "gabipaval17@gmail.com");
+            string s4 = ob4.InfoPersoana();
+            Console.WriteLine(s4);
+
+            Console.ReadKey();
+
         }
     }
 }
